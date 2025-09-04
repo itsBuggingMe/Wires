739f5d755674f5855aae09d7d87feda727754624
Paperclip <lohj002@gmail.com>
2025-09-04 11:39:24 +0000

    remove unneeded directories

--- Wires.Desktop/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace Wires;
-
-public static class Constants
-{
-    public static readonly Color Background = new(32, 28, 28);
-    public static readonly Color Accent = new(0x2b2b2eu);
-}
